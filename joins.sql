6b2fe29a8437cb85670aec94524f3adb7c7e229f
Andrea Takamiya <Andrea@Andreas-Air.T-mobile.com>
2017-01-25 07:37:14 -1000

    Finished #7

--- joins.sql
+++ joins.sql
@@ -1,31 +1,37 @@
--- #1
-SELECT * FROM users;
+-- -- #1
+-- SELECT * FROM users;
 
--- #2
-SELECT * FROM posts
-    WHERE users_id = 100;
+-- -- #2
+-- SELECT * FROM posts
+--     WHERE users_id = 100;
 
--- #3
-SELECT first_name, last_name, posts.*
+-- -- #3
+-- SELECT first_name, last_name, posts.*
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--     WHERE users_id = 200;
+
+-- -- #4
+-- SELECT username, posts.*
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--     WHERE first_name = 'Norene' AND last_name = 'Schmitt';
+
+-- -- #4
+-- SELECT users.username
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--     WHERE posts.created_at > TIMESTAMP WITH TIME ZONE '2015-01-01';
+
+-- -- #5
+-- SELECT posts.title, posts.content, users.username
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--     WHERE users.created_at > TIMESTAMP WITH TIME ZOqNE '2015-01-01';
+
+-- #6
+SELECT posts.title, comments.*
   FROM users
   INNER JOIN posts ON posts.users_id = users.id
-    WHERE users_id = 200;
-
--- #4
-SELECT username, posts.*
-  FROM users
-  INNER JOIN posts ON posts.users_id = users.id
-    WHERE first_name = 'Norene' AND last_name = 'Schmitt';
-
--- #4
-SELECT users.username
-  FROM users
-  INNER JOIN posts ON posts.users_id = users.id
-    WHERE posts.created_at > TIMESTAMP WITH TIME ZONE '2015-01-01';
-
--- #5
-SELECT posts.title, posts.content, users.username
-  FROM users
-  INNER JOIN posts ON posts.users_id = users.id
-    WHERE users.created_at > TIMESTAMP WITH TIME ZONE '2015-01-01';
+  INNER JOIN comments ON comments.posts_id = posts.id;
 
