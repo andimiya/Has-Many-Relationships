177417a26df9590328f327191172045c4701b5bf
Andrea Takamiya <Andrea@Andreas-MacBook-Air.local>
2017-01-25 19:25:47 -1000

    Additional Query #3 done, added some timezones to other queries

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,5 +1,8 @@
+\c postgres
+
 DROP DATABASE IF EXISTS has_many_blogs;
 DROP USER IF EXISTS has_many_user;
+
 CREATE USER has_many_user;
 CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
 
--- joins.sql
+++ joins.sql
@@ -1,85 +1,106 @@
--- #1
-SELECT * FROM users;
+-- -- #1
+-- SELECT * FROM users;
 
--- #2
-SELECT * FROM posts
-    WHERE users_id = 100;
+-- -- #2
+-- SELECT * FROM posts
+--     WHERE users_id = 100;
 
--- #3
-SELECT first_name, last_name, posts.*
-  FROM users
-  INNER JOIN posts ON posts.users_id = users.id
-    WHERE users_id = 200;
+-- -- #3
+-- SELECT first_name, last_name, posts.*
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--     WHERE users_id = 200;
 
--- #4
-SELECT username, posts.*
-  FROM users
-  INNER JOIN posts ON posts.users_id = users.id
-    WHERE first_name = 'Norene' AND last_name = 'Schmitt';
+-- -- #4
+-- SELECT username, posts.*
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--     WHERE first_name = 'Norene' AND last_name = 'Schmitt';
 
--- #5
-SELECT users.username, posts.created_at
-  FROM users
-  INNER JOIN posts ON posts.users_id = users.id
-    WHERE posts.created_at > TIMESTAMP WITH TIME ZONE '2015-01-01';
+-- -- #5
+-- SELECT users.username, posts.created_at
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--     WHERE posts.created_at > TIMESTAMP WITH TIME ZONE '2015-01-01';
 
--- #6
-SELECT posts.title, posts.content, users.username
-  FROM users
-  INNER JOIN posts ON posts.users_id = users.id
-    WHERE users.created_at > TIMESTAMP WITH TIME ZOqNE '2015-01-01';
+-- -- #6
+-- SELECT posts.title, posts.content, users.username
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--     WHERE users.created_at > TIMESTAMP WITH TIME ZONE '2015-01-01 00:00:00-10';
 
--- #7
-SELECT posts.title AS "Post Title", comments.*
-  FROM users
-  INNER JOIN posts ON posts.users_id = users.id
-  INNER JOIN comments ON comments.posts_id = posts.id;
+-- -- #7
+-- SELECT posts.title AS "Post Title", comments.*
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--   INNER JOIN comments ON comments.posts_id = posts.id;
 
--- #8
-SELECT posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body
+-- -- #8
+-- SELECT posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--   INNER JOIN comments ON comments.posts_id = posts.id
+--     WHERE posts.created_at < TIMESTAMP WITH TIME ZONE '2015-01-01 00:00:00-10';
+
+-- -- #9
+-- SELECT posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--   INNER JOIN comments ON comments.posts_id = posts.id
+--     WHERE posts.created_at > TIMESTAMP WITH TIME ZONE '2015-01-01';
+
+-- -- #10
+-- SELECT posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--   INNER JOIN comments ON comments.posts_id = posts.id
+--     WHERE comments.body LIKE '%USB%';
+
+-- -- #11 (855 Results)
+-- SELECT posts.title AS post_title, users.first_name, users.last_name, comments.body AS comment_body
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--   INNER JOIN comments ON comments.posts_id = posts.id
+--     WHERE comments.body LIKE '%matrix%';
+
+-- -- #12 (102 Results)
+-- SELECT users.first_name, users.last_name, comments.body AS comment_body
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--   INNER JOIN comments ON comments.posts_id = posts.id
+--     WHERE comments.body LIKE '%SSL%'
+--     AND posts.content LIKE '%dolorum%';
+
+-- -- #13 (218 Results)
+-- SELECT users.first_name AS post_author_first_name,
+--   users.last_name AS post_author_last_name,
+--   posts.title AS post_title,
+--   users.username AS comment_author_username,
+--   comments.body AS comment_body
+--     FROM users
+--     INNER JOIN posts ON posts.users_id = users.id
+--     INNER JOIN comments ON comments.posts_id = posts.id
+--       WHERE (comments.body LIKE '%SSL%'
+--       OR  comments.body LIKE '%firewall%')
+--       AND posts.content LIKE '%nemo%';
+
+-- --Additional Query #1
+-- SELECT posts.id, posts.title, users.id
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--   INNER JOIN comments ON comments.posts_id = posts.id
+--     WHERE users.username = comments.id;
+
+--Additional Query #2
+SELECT comments.created_at, comments.body
   FROM users
   INNER JOIN posts ON posts.users_id = users.id
   INNER JOIN comments ON comments.posts_id = posts.id
-    WHERE posts.created_at < TIMESTAMP WITH TIME ZONE '2015-01-01';
+    WHERE posts.created_at > TIMESTAMP WITH TIME ZONE '2015-07-14 00:00:00-10';
 
--- #9
-SELECT posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body
-  FROM users
-  INNER JOIN posts ON posts.users_id = users.id
-  INNER JOIN comments ON comments.posts_id = posts.id
-    WHERE posts.created_at > TIMESTAMP WITH TIME ZONE '2015-01-01';
-
--- #10
-SELECT posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body
-  FROM users
-  INNER JOIN posts ON posts.users_id = users.id
-  INNER JOIN comments ON comments.posts_id = posts.id
-    WHERE comments.body LIKE '%USB%';
-
--- #11 (855 Results)
-SELECT posts.title AS post_title, users.first_name, users.last_name, comments.body AS comment_body
-  FROM users
-  INNER JOIN posts ON posts.users_id = users.id
-  INNER JOIN comments ON comments.posts_id = posts.id
-    WHERE comments.body LIKE '%matrix%';
-
--- #12 (102 Results)
-SELECT users.first_name, users.last_name, comments.body AS comment_body
-  FROM users
-  INNER JOIN posts ON posts.users_id = users.id
-  INNER JOIN comments ON comments.posts_id = posts.id
-    WHERE comments.body LIKE '%SSL%'
-    AND posts.content LIKE '%dolorum%';
-
--- #13 (218 Results)
-SELECT users.first_name AS post_author_first_name,
-  users.last_name AS post_author_last_name,
-  posts.title AS post_title,
-  users.username AS comment_author_username,
-  comments.body AS comment_body
-    FROM users
-    INNER JOIN posts ON posts.users_id = users.id
-    INNER JOIN comments ON comments.posts_id = posts.id
-      WHERE (comments.body LIKE '%SSL%'
-      OR  comments.body LIKE '%firewall%')
-      AND posts.content LIKE '%nemo%';
+-- --Additional Query #3
+-- SELECT users.username
+--   FROM users
+--   INNER JOIN posts ON posts.users_id = users.id
+--   INNER JOIN comments ON comments.posts_id = posts.id
+--     WHERE comments.body LIKE '%programming%';
